6b6cd9e6cd6538db2dfc3c9adf86b78922dcd465
Akshat <codedakshat@gmail.com>
2025-08-14 23:59:31 +0530

    Mark 8 - Runnable dvclive code with debugs

--- src/model_building.py
+++ src/model_building.py
@@ -28,22 +28,23 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-def load_params(params_path : str) -> dict :
-    """Load parameters with yaml file"""
-    try :
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
         with open(params_path, 'r') as file:
             params = yaml.safe_load(file)
-        logger.debug('Parameters retrieved')
+        logger.debug('Parameters retrieved from %s', params_path)
         return params
     except FileNotFoundError:
-        logger.error('File Not Found error')
+        logger.error('File not found: %s', params_path)
         raise
-    except yaml.YAMLError :
-        logger.error('Yaml error')
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
         raise
     except Exception as e:
-        logger.error('Error unexpected')
-        print(f"Error : {e}")
+        logger.error('Unexpected error: %s', e)
+        raise
+
 
 def load_data(file_path: str) -> pd.DataFrame:
     """
@@ -118,8 +119,7 @@ def save_model(model, file_path: str) -> None:
 
 def main():
     try:
-        parameters = load_params('params.yaml')
-        params = parameters['model_building']
+        params = load_params('params.yaml')['model_building']
         train_data = load_data('./data/processed/train_tfidf.csv')
         X_train = train_data.iloc[:, :-1].values
         y_train = train_data.iloc[:, -1].values
--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -5,19 +5,21 @@ import pickle
 import json
 from sklearn.metrics import accuracy_score, precision_score, recall_score, roc_auc_score
 import logging
+import yaml
+from dvclive import Live
 
 # Ensure the "logs" directory exists
 log_dir = 'logs'
 os.makedirs(log_dir, exist_ok=True)
 
-# Setting up logger
-logger = logging.getLogger('data_preprocessing')
+# logging configuration
+logger = logging.getLogger('model_evaluation')
 logger.setLevel('DEBUG')
 
 console_handler = logging.StreamHandler()
 console_handler.setLevel('DEBUG')
 
-log_file_path = os.path.join(log_dir, 'data_preprocessing.log')
+log_file_path = os.path.join(log_dir, 'model_evaluation.log')
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel('DEBUG')
 
@@ -28,8 +30,24 @@ file_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
-def load_model(file_path:str) : 
+def load_model(file_path: str):
     """Load the trained model from a file."""
     try:
         with open(file_path, 'rb') as file:
@@ -37,28 +55,28 @@ def load_model(file_path:str) :
         logger.debug('Model loaded from %s', file_path)
         return model
     except FileNotFoundError:
-        logger.error('File not found')
-        raise 
+        logger.error('File not found: %s', file_path)
+        raise
     except Exception as e:
-        logger.error('Unexpected error occured: %s', e)
+        logger.error('Unexpected error occurred while loading the model: %s', e)
         raise
 
-def load_data(file_path : str) -> pd.DataFrame:
-    """Load the data from a file."""
-    try :
+def load_data(file_path: str) -> pd.DataFrame:
+    """Load data from a CSV file."""
+    try:
         df = pd.read_csv(file_path)
         logger.debug('Data loaded from %s', file_path)
         return df
-    except pd.errors.ParserError as e :
-        logger.error('Failed to parse the csv file &s', e)
+    except pd.errors.ParserError as e:
+        logger.error('Failed to parse the CSV file: %s', e)
         raise
     except Exception as e:
-        logger.error('Unexpected error occured: %s', e)
+        logger.error('Unexpected error occurred while loading the data: %s', e)
         raise
 
-def evaluate_model(clf, X_test : np.ndarray, y_test : np.ndarray) -> dict :
-    """Evaluate the model and its metrics"""
-    try :
+def evaluate_model(clf, X_test: np.ndarray, y_test: np.ndarray) -> dict:
+    """Evaluate the model and return the evaluation metrics."""
+    try:
         y_pred = clf.predict(X_test)
         y_pred_proba = clf.predict_proba(X_test)[:, 1]
 
@@ -68,44 +86,53 @@ def evaluate_model(clf, X_test : np.ndarray, y_test : np.ndarray) -> dict :
         auc = roc_auc_score(y_test, y_pred_proba)
 
         metrics_dict = {
-            'accuracy' : accuracy,
-            'precision' : precision,
-            'recall' : recall,
-            'auc' : auc
+            'accuracy': accuracy,
+            'precision': precision,
+            'recall': recall,
+            'auc': auc
         }
-
         logger.debug('Model evaluation metrics calculated')
         return metrics_dict
-    
     except Exception as e:
-        logger.error('Error during model evaluation %s', e)
+        logger.error('Error during model evaluation: %s', e)
         raise
 
-
-def save_metrics(metrics : dict, file_path : str) -> None :
-    """Save the evaluation metrics in a JSON file"""
-    try :
+def save_metrics(metrics: dict, file_path: str) -> None:
+    """Save the evaluation metrics to a JSON file."""
+    try:
+        # Ensure the directory exists
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
+
         with open(file_path, 'w') as file:
-            json.dump(metrics, file, indent = 4)
-        logger.debug('Metrics saved to path %s : ', file_path)
+            json.dump(metrics, file, indent=4)
+        logger.debug('Metrics saved to %s', file_path)
     except Exception as e:
-        logger.error('Error occured while saving the file %s', e)
+        logger.error('Error occurred while saving the metrics: %s', e)
         raise
 
 def main():
-    try :
+    try:
+        params = load_params(params_path='params.yaml')
         clf = load_model('./models/model.pkl')
         test_data = load_data('./data/processed/test_tfidf.csv')
+        
         X_test = test_data.iloc[:, :-1].values
         y_test = test_data.iloc[:, -1].values
 
         metrics = evaluate_model(clf, X_test, y_test)
+
+        # Experiment tracking using dvclive
+        with Live("dvclive", save_dvc_exp=True) as live:
+            live.log_metric('accuracy', accuracy_score(y_test, y_test))
+            live.log_metric('precision', precision_score(y_test, y_test))
+            live.log_metric('recall', recall_score(y_test, y_test))
+
+            live.log_params(params)
+        
         save_metrics(metrics, 'reports/metrics.json')
     except Exception as e:
-        logger.error('Unexpeceted error happened during the execution %s', e)
-        print(f"Error : {e}")
-
+        logger.error('Failed to complete the model evaluation process: %s', e)
+        print(f"Error: {e}")
 
 if __name__ == '__main__':
     main()
